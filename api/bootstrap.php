61392c7c92aa20c9c0051a81bc450569059e39f8
Nicolas DENIS <contact@ndi.dev>
2024-10-09 17:38:52 +0200

    Fix Constants path

--- api/bootstrap.php
+++ api/bootstrap.php
@@ -9,7 +9,7 @@ define('UNIFONTS', FONTS . '/unifont');
 define('LOGOS', ROOTPATH . '/logos');
 
 require_once __DIR__ . "/vendor/autoload.php";
-require_once __DIR__ . "/src/Core/Constants.php";
+require_once __DIR__ . "/src/Core/Component/Constants.php";
 
 // Chargement des variables d'environnement
 $dotenv = Dotenv\Dotenv::createImmutable(ROOTPATH, ".env");
